158b2a6840a777ac2ec7e0271762274904f0ea39
Арсений Несытых <arsen.nesytykh@gmail.com>
2022-04-06 06:20:16 +0300

    Добавьте файлы проекта.

---
+++ asm4/asm4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+extern "C" int __stdcall SumMinMax(int*, int n);
+extern "C" int __fastcall SumMinMax3(int*, int n);
+extern "C" int __cdecl SumMinMax2(int*, int n);
+extern "C" int __stdcall Min(int*, int n);
+extern "C" int __cdecl Max(int*, int n);
+
+int main()
+{
+    int n = 10;
+
+    int* b = new int[n];
+    srand(time(0));
+    for (int i = 0; i < n; i++) {
+        b[i] = rand() % 100;
+        cout << b[i] << " ";
+    }
+    cout << endl;
+
+
+    int max = 0;
+    int min = 0;
+    int n1 = n - 1;
+
+    cout << "min=" << Min(b, n1) << endl;
+    cout << "max=" << Max(b, n1) << endl;
+    cout << "(stdcall) sum=" << SumMinMax(b, n1) << endl;  
+    cout << "(cdecl) sum=" << SumMinMax2(b, n1) << endl;
+    cout << "(fastcall) sum=" << SumMinMax3(b, n1) << endl;
+}
---
+++ asm4/f.asm
@@ -0,0 +1,183 @@
+.486
+
+PUBLIC @SumMinMax3@8
+PUBLIC SumMinMax
+PUBLIC _SumMinMax2
+PUBLIC _Min@8
+PUBLIC Max
+
+
+.model flat
+.data
+.code
+
+@SumMinMax3@8 proc
+    push esi    
+    push edi
+
+    mov ebx,ecx ;//занесение адреса массива b          
+    mov ecx,edx
+    mov esi,ebx ;// min address
+    mov eax,[esi] ;// min value
+    mov edi,ebx ;// max address
+    mov edx,eax ;// max value
+        
+    add ebx,4 
+l1 :
+    cmp eax, [ebx] ;// сравниваем минимум с текущим
+    jle e1 ;// если (eax < [ebx] == true) меньше 1й, то jmp to e1
+    mov esi,ebx
+    mov eax,[ebx]
+    jmp e2
+e1:
+    cmp edx, [ebx]
+    jge e2
+    mov edi, ebx
+    mov edx, [ebx]
+e2 :
+    add ebx, 4
+    loop l1
+
+    xor eax, eax
+
+    cmp esi, edi
+    jl e3
+    mov ebx, edi            
+    mov ecx, esi
+    jmp l2
+e3 : 
+    mov ebx, esi
+    mov ecx, edi    
+
+l2:
+    add eax,[ebx]
+    add ebx,4
+    cmp ebx,ecx
+    jle l2
+
+    pop edi
+    pop esi
+    ret
+@SumMinMax3@8 endp
+
+; *********************************************
+
+_SumMinMax2 proc
+push ebp
+mov ebp, esp
+push [ebp+12]
+push [ebp+8]
+call SumMinMax
+mov esp, ebp
+pop ebp
+ret
+_SumMinMax2 endp
+
+; *********************************************
+
+SumMinMax proc stdcall, a: dword, b: dword
+    push esi    
+    push edi
+
+    mov ebx,a ;//занесение адреса массива b          
+    mov ecx,b
+    mov esi,ebx ;// min address
+    mov eax,[esi] ;// min value
+    mov edi,ebx ;// max address
+    mov edx,eax ;// max value
+        
+    add ebx,4 
+l1 :
+    cmp eax, [ebx] ;// сравниваем минимум с текущим
+    jle e1 ;// если (eax < [ebx] == true) меньше 1й, то jmp to e1
+    mov esi,ebx
+    mov eax,[ebx]
+    jmp e2
+e1:
+    cmp edx, [ebx]
+    jge e2
+    mov edi, ebx
+    mov edx, [ebx]
+e2 :
+    add ebx, 4
+    loop l1
+
+    xor eax, eax
+
+    cmp esi, edi
+    jl e3
+    mov ebx, edi            
+    mov ecx, esi
+    jmp l2
+e3 : 
+    mov ebx, esi
+    mov ecx, edi    
+
+l2:
+    add eax,[ebx]
+    add ebx,4
+    cmp ebx,ecx
+    jle l2
+
+    pop edi
+    pop esi
+    ret
+SumMinMax endp
+
+; *********************************************
+
+_Min@8 proc
+    push ebp
+    mov ebp,esp
+    push esi    
+    push edi
+
+    mov ebx,[ebp+8] ;//занесение адреса массива b          
+    mov ecx,[ebp+12]
+    mov esi,ebx ;// min address
+    mov eax,[esi] ;// min value   
+        
+    add ebx,4 
+l1 :
+    cmp eax, [ebx] ;// сравниваем минимум с текущим
+    jle e1 ;// если (eax < [ebx] == true) меньше 1й, то jmp to e1
+    mov esi,ebx
+    mov eax,[ebx]  
+e1:
+    add ebx, 4
+    loop l1
+
+    pop edi
+    pop esi
+    mov esp, ebp
+    pop ebp
+    ret 8
+_Min@8 endp
+
+; *********************************************
+
+Max proc C, a:dword, b:dword    
+    push esi    
+    push edi
+
+    mov ebx,a ;//занесение адреса массива b          
+    mov ecx,b
+    mov esi,ebx ;// max address
+    mov eax,[esi] ;// max value   
+        
+    add ebx,4 
+l1 :
+    cmp eax, [ebx] ;// сравниваем минимум с текущим
+    jge e1 ;// если (eax > [ebx] == true)
+    mov esi,ebx
+    mov eax,[ebx]  
+e1:
+    add ebx, 4
+    loop l1
+
+    pop edi
+    pop esi    
+    ret
+Max endp
+
+end
